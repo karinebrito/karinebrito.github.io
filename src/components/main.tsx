6b29353dc59161d967f563f1ce86b12180712c3a
Karine Brito <annakarinebrito@gmail.com>
2024-05-07 18:15:14 -0300

    adding yaml

---
+++ src/components/main.tsx
@@ -0,0 +1,19 @@
+import Nav from './nav'
+import Hero from './hero'
+import Experience from './experience'
+import Contact from './contact'
+import Footer from './footer'
+
+const Main = () => {
+  return (
+    <div className='bg-black'>
+      <Nav />
+      <Hero />
+      <Experience />
+      <Contact />
+      <Footer />
+    </div>
+  )
+}
+
+export default Main
