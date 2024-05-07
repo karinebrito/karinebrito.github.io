1343d7378ef957dacba7a0bbd430ae1fa4ce2162
Karine Brito <annakarinebrito@gmail.com>
2024-05-07 17:30:11 -0300

    correcting the code structure

---
+++ App.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Nav from './src/components/nav.tsx'
+import Hero from './src/components/hero.tsx'
+import Experience from './src/components/experience.tsx'
+import Contact from './src/components/contact.tsx'
+import Footer from './src/components/footer.tsx'
+import './src/i18n.tsx'
+
+function App() {
+  return (
+    <>
+      <div className='bg-black'>
+        <Nav />
+        <Hero />
+        <Experience />
+        <Contact />
+        <Footer />
+      </div>
+    </>
+  )
+}
+
+export default App
