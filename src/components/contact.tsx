071de187a8b5e7c1e8c137d9732b4107f9353c39
Karine Brito <annakarinebrito@gmail.com>
2024-05-15 18:53:40 -0300

    changing footer style

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -1,4 +1,3 @@
-import { BsEnvelopeAt } from 'react-icons/bs'
 import { useTranslation } from 'react-i18next'
 import Footer from './footer'
 
@@ -10,18 +9,20 @@ const Contact = () => {
   }
 
   return (
-    <section id='contact-section' className='flex flex-col justify-center items-left p-10 lg:px-32 lg:min-h-screen'>
-        <h2 className='text-xl text-white mb-4 xs:mt-20' >{t('contact.contact_me')}</h2>
+    <section id='contact-section' className='bg-gray-900 flex flex-col justify-center items-left p-10 lg:px-32'>
+        <h2 className='text-xl text-white xs:mt-20' >{t('contact.contact_me')}</h2>
         <div className='xs:mt-4'>
             <p className='text-slate-300'>
             {t('contact.doubt')}
             </p>
         </div>
-        <div className='text-white mt-2 cursor-pointer'>
-            <BsEnvelopeAt size={60} className='hover:text-cyan-500 hover:scale-105 transition-transform duration-200 ease-in-out' onClick={openEmail}/>
-        </div>
-        <div className='mt-20'>
-            <Footer />
+        <div>
+            <button onClick={openEmail} className='mt-4 p-4 text-white border-2 rounded-md transition-transform transform hover:scale-110'>
+                Diga Oi!
+            </button>
+            <div className='mt-20'>
+                <Footer />
+            </div>
         </div>
     </section>
   )
